64a7578449b393e399af747f4d572092f32f0db3
mingquanjp <faker1652005@gmail.com>
2025-06-14 01:25:50 +0700

    update form for orders

--- ecommerce/orders/urls.py
+++ ecommerce/orders/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('place_order/', views.place_order, name='place_order'),
+    path('payment/<int:order_id>/', views.payment_page, name='payment_page'),
+    path('vnpay-payment/<int:order_id>/', views.vnpay_payment, name='vnpay_payment'),
+    path('vnpay-return/', views.vnpay_return, name='vnpay_return'),
+    path('vnpay-ipn/', views.vnpay_ipn, name='vnpay_ipn'),  # Thêm dòng này
+    path('payment-success/<str:order_number>/', views.payment_success, name='payment_success'),
+    path('payment-failed/<str:order_number>/', views.payment_failed, name='payment_failed'),
     #path('payments/', views.payments, name='payments'),
     #path('order_complete/', views.order_complete, name='order_complete'),
 ]
